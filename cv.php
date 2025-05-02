82c79ac2f9c0ae2f3e0b2f996fa48af040ddf200
BobZouille57 <RaiiZeR57@gmail.com>
2025-05-02 15:16:20 +0200

    test

--- cv.php
+++ cv.php
@@ -9,7 +9,8 @@
     <button id="toggle-cv" class="btn-toggle">👁️ Afficher / Masquer le CV</button>
 
     <div class="cv-viewer" style="display: none;">
-        <iframe src="assets/pdf/cv.pdf" frameborder="0"></iframe>
+    <iframe src="https://docs.google.com/gview?url=http://clementpierron.fr/assets/pdf/cv.pdf&embedded=true" style="width:100%; height:600px;" frameborder="0"></iframe>
+
     </div>
 
     <a href="assets/pdf/cv.pdf" class="btn-download" download>

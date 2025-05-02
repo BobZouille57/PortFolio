20a2bf78ac8a1469a63d5855d5faedf6f8b7e278
BobZouille57 <RaiiZeR57@gmail.com>
2025-05-02 14:41:02 +0200

    fix

--- includes/footer.php
+++ includes/footer.php
@@ -3,10 +3,10 @@
     <div class="footer-container">
         <p>&copy; <?php echo date("Y"); ?> Mon Portfolio - Tous droits réservés</p>
         <div class="social-icons">
-            <a href="https://www.linkedin.com/in/ton-profile" target="_blank">
+            <a href="https://www.linkedin.com/in/clementpi/" target="_blank">
                 <img src="images/Linkedin-icon.png" alt="LinkedIn">
             </a>
-            <a href="https://github.com/ton-profile" target="_blank">
+            <a href="https://github.com/BobZouille57" target="_blank">
                 <img src="images/Github-icon.png" alt="GitHub">
             </a>
         </div>

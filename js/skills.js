8ffbdda9ef2a2ec0c00dd5b1660af155bc7ae8a4
BobZouille57 <RaiiZeR57@gmail.com>
2025-05-09 11:50:19 +0200

    skills v3

--- js/skills.js
+++ js/skills.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('.progress').each(function() {
         var width = $(this).css('width');
-        $(this).animate({ width: width }, 1500);
+        $(this).animate({ width: width }, 2000);
     });
 });
--- skills.php
+++ skills.php
@@ -1,58 +1,49 @@
 <?php include('includes/header.php'); ?>
 <?php include('includes/navbar.php'); ?>
-
 <div class="skills-container">
     <h2>Mes Compétences</h2>
 
-    <div class="skill">
-        <div class="skill-header">
-            <i class="fab fa-php"></i>
-            <h3>PHP</h3>
-        </div>
-        <p class="skill-description">Je maîtrise PHP pour le développement de sites web dynamiques et d'applications web. Utilisation de PHP pour la gestion de bases de données, la création de formulaires et l'authentification des utilisateurs.</p>
+    <div class="skill" id="php">
+        <h3>PHP</h3>
+        <p>Développement back-end avec PHP. Réalisation de projets web dynamiques, intégration avec des bases de données (MySQL), gestion des utilisateurs et des systèmes de connexion.</p>
         <div class="progress-bar">
             <div class="progress" style="width: 80%;"></div>
         </div>
-        <div class="skill-info">
-            <p>Expérience : 2 ans</p>
-            <p>Projets : <a href="https://github.com/BobZouille57/Helpdesk" target="_blank">HelpDesk</a>, <a href="https://github.com/BobZouille57/Mini-Projet" target="_blank">Mini-Projet Restaurant</a></p>
-        </div>
     </div>
 
-    <div class="skill">
-        <div class="skill-header">
-            <i class="fab fa-html5"></i>
-            <h3>HTML / CSS</h3>
-        </div>
-        <p class="skill-description">Création de pages web structurées et esthétiques avec HTML5 et CSS3. Maîtrise de la mise en page avec Flexbox, Grid, ainsi que de la responsivité pour différents écrans.</p>
+    <div class="skill" id="html-css">
+        <h3>HTML / CSS</h3>
+        <p>Création de structures de pages web responsive et design moderne en utilisant HTML et CSS. Mise en place de sites optimisés et agréables visuellement.</p>
         <div class="progress-bar">
             <div class="progress" style="width: 90%;"></div>
         </div>
-        <div class="skill-info">
-            <p>Expérience : 3 ans</p>
-            <p>Projets : <a href="https://github.com/BobZouille57/PortFolio" target="_blank">Portfolio</a>, <a href="https://github.com/BobZouille57/JCVD-PCl-ment" target="_blank">JCVD</a></p>
-        </div>
     </div>
 
-    <div class="skill">
-        <div class="skill-header">
-            <i class="fab fa-js-square"></i>
-            <h3>JavaScript</h3>
-        </div>
-        <p class="skill-description">Utilisation de JavaScript pour rendre les sites interactifs. Création d'animations, d'interactions utilisateur et de logique de jeu avec des projets comme Chifoumi.</p>
+    <div class="skill" id="javascript">
+        <h3>JavaScript</h3>
+        <p>Utilisation de JavaScript pour dynamiser les pages web, ajout d'interactivité avec des animations et manipulations DOM. Quelques projets réalisés en JS pur et en jQuery.</p>
         <div class="progress-bar">
             <div class="progress" style="width: 75%;"></div>
         </div>
-        <div class="skill-info">
-            <p>Expérience : 1 an</p>
-            <p>Projets : <a href="https://github.com/BobZouille57/Exercice-JS" target="_blank">Chifoumi</a></p>
+    </div>
+
+    <div class="skill" id="laravel">
+        <h3>Laravel</h3>
+        <p>Développement d'applications web avec le framework PHP Laravel. Création de back-office, gestion de bases de données et création d'APIs RESTful durant mon stage chez MY.AND.COMMUNITY.</p>
+        <div class="progress-bar">
+            <div class="progress" style="width: 70%;"></div>
         </div>
     </div>
 
+    <div class="skill" id="mysql">
+        <h3>MySQL</h3>
+        <p>Gestion de bases de données avec MySQL, création de tables, requêtes SQL complexes pour le traitement de données et optimisation des performances.</p>
+        <div class="progress-bar">
+            <div class="progress" style="width: 80%;"></div>
+        </div>
+    </div>
 </div>
-
 <?php include('includes/footer.php'); ?>
 
 <link rel="stylesheet" href="assets/css/skills.css">
-<script src="https://kit.fontawesome.com/a076d05399.js"></script>
 <script src="js/skills.js"></script>

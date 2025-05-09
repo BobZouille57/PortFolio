6955120fd7a9dbd36012b8b95f49e58e5cad2bc7
BobZouille57 <RaiiZeR57@gmail.com>
2025-05-09 11:44:07 +0200

    v2 skills

--- skills.php
+++ skills.php
@@ -1,27 +1,58 @@
 <?php include('includes/header.php'); ?>
 <?php include('includes/navbar.php'); ?>
+
 <div class="skills-container">
     <h2>Mes Compétences</h2>
+
     <div class="skill">
-        <h3>PHP</h3>
+        <div class="skill-header">
+            <i class="fab fa-php"></i>
+            <h3>PHP</h3>
+        </div>
+        <p class="skill-description">Je maîtrise PHP pour le développement de sites web dynamiques et d'applications web. Utilisation de PHP pour la gestion de bases de données, la création de formulaires et l'authentification des utilisateurs.</p>
         <div class="progress-bar">
             <div class="progress" style="width: 80%;"></div>
         </div>
+        <div class="skill-info">
+            <p>Expérience : 2 ans</p>
+            <p>Projets : <a href="https://github.com/BobZouille57/Helpdesk" target="_blank">HelpDesk</a>, <a href="https://github.com/BobZouille57/Mini-Projet" target="_blank">Mini-Projet Restaurant</a></p>
+        </div>
     </div>
+
     <div class="skill">
-        <h3>HTML / CSS</h3>
+        <div class="skill-header">
+            <i class="fab fa-html5"></i>
+            <h3>HTML / CSS</h3>
+        </div>
+        <p class="skill-description">Création de pages web structurées et esthétiques avec HTML5 et CSS3. Maîtrise de la mise en page avec Flexbox, Grid, ainsi que de la responsivité pour différents écrans.</p>
         <div class="progress-bar">
             <div class="progress" style="width: 90%;"></div>
         </div>
+        <div class="skill-info">
+            <p>Expérience : 3 ans</p>
+            <p>Projets : <a href="https://github.com/BobZouille57/PortFolio" target="_blank">Portfolio</a>, <a href="https://github.com/BobZouille57/JCVD-PCl-ment" target="_blank">JCVD</a></p>
+        </div>
     </div>
+
     <div class="skill">
-        <h3>JavaScript</h3>
+        <div class="skill-header">
+            <i class="fab fa-js-square"></i>
+            <h3>JavaScript</h3>
+        </div>
+        <p class="skill-description">Utilisation de JavaScript pour rendre les sites interactifs. Création d'animations, d'interactions utilisateur et de logique de jeu avec des projets comme Chifoumi.</p>
         <div class="progress-bar">
             <div class="progress" style="width: 75%;"></div>
         </div>
+        <div class="skill-info">
+            <p>Expérience : 1 an</p>
+            <p>Projets : <a href="https://github.com/BobZouille57/Exercice-JS" target="_blank">Chifoumi</a></p>
+        </div>
     </div>
+
 </div>
+
 <?php include('includes/footer.php'); ?>
 
 <link rel="stylesheet" href="assets/css/skills.css">
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
 <script src="js/skills.js"></script>

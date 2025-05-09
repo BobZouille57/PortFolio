e5f0799c6e63afbd6044dbb458cbbce4dfc70170
BobZouille57 <RaiiZeR57@gmail.com>
2025-05-09 10:18:49 +0200

    test

--- js/about.js
+++ js/about.js
@@ -1,46 +1,21 @@
 $(document).ready(function () {
-    function revealOnScroll() {
-        $('.reveal').each(function () {
-            const elementTop = $(this).offset().top;
-            const scrollTop = $(window).scrollTop();
-            const windowHeight = $(window).height();
+    function showOnlyVisibleBlock() {
+        $('.intro-block, .journey-block, .timeline-item, .conclusion-block, .profile-img').each(function () {
+            const blockTop = $(this).offset().top;
+            const blockBottom = blockTop + $(this).outerHeight();
+            const windowTop = $(window).scrollTop();
+            const windowBottom = windowTop + $(window).height();
 
-            if (scrollTop + windowHeight > elementTop + 50) {
-                $(this).animate({ opacity: 1 }, {
-                    duration: 600,
-                    step: function (now, fx) {
-                        $(this).css('transform', 'scale(' + (0.95 + (now * 0.05)) + ')');
-                    }
-                });
+            if (blockTop < windowBottom && blockBottom > windowTop) {
+                // Bloc visible à l'écran
+                $(this).fadeIn(600);
+            } else {
+                // Bloc hors de l'écran
+                $(this).fadeOut(400);
             }
         });
     }
 
-    $('.reveal').css({
-        opacity: 0,
-        transform: 'scale(0.95)'
-    });
-
-    revealOnScroll();
-    $(window).on('scroll', function () {
-        revealOnScroll();
-    });
-});
-
-let lastScrollTop = 0;
-
-$(window).on("scroll", function () {
-    let st = $(this).scrollTop();
-
-    if (st > lastScrollTop) {
-        // On descend
-        $(".intro-block").slideUp();
-        $(".conclusion-block").slideDown();
-    } else {
-        // On remonte
-        $(".intro-block").slideDown();
-        $(".conclusion-block").slideUp();
-    }
-
-    lastScrollTop = st;
+    $(window).on('scroll resize', showOnlyVisibleBlock);
+    showOnlyVisibleBlock(); // initial
 });

28f7429e56a9cd7bad7b4dfd9fc2fc1a39905f79
BobZouille57 <RaiiZeR57@gmail.com>
2025-05-09 09:59:32 +0200

    fix about

--- about.php
+++ about.php
@@ -1,38 +1,48 @@
 <?php include('includes/header.php'); ?>
 <?php include('includes/navbar.php'); ?>
+
 <div class="about-container">
     <h2>À propos de moi</h2>
-    <p class="intro-text">Je m'appelle Clément, j'ai 22 ans, et je suis passionné par le développement informatique, en particulier le développement web. Depuis mon plus jeune âge, j'ai toujours été fasciné par la manière dont les technologies peuvent changer et améliorer notre quotidien. Cela m'a poussé à m'orienter vers un parcours scolaire et professionnel qui allie ma passion pour l'informatique et ma volonté de résoudre des problèmes concrets à travers des solutions digitales.</p>
-    
-    <p class="journey-text">Mon parcours en informatique a commencé dès le collège, où, à l'âge de 14 ans, j'ai commencé à m'intéresser à la programmation et à l'usage des technologies numériques. J'ai suivi un parcours scolaire dans lequel l'informatique est toujours restée un axe majeur. Voici un aperçu de mon parcours :</p>
-    
-    <div class="timeline">
-        <div class="timeline-item">
-            <span class="year">2014 - 2018</span>
-            <p class="school">Brevet des collèges - Collège Jacques Monod KONACKER</p>
-        </div>
-        <div class="timeline-item">
-            <span class="year">2018 - 2019</span>
-            <p class="school">Seconde générale - Lycée Antoine de Saint-Exupéry FAMECK</p>
-        </div>
-        <div class="timeline-item">
-            <span class="year">2019 - 2021</span>
-            <p class="school">Baccalauréat STI2D (option SIN) - Lycée La Briquerie THIONVILLE</p>
-        </div>
-        <div class="timeline-item">
-            <span class="year">2021</span>
-            <p class="school">BTS SNIR - Lycée La Briquerie THIONVILLE</p>
-        </div>
-        <div class="timeline-item">
-            <span class="year">2023 - 2025</span>
-            <p class="school">BTS SIO (SLAM) - Campus MEWO METZ</p>
+    <div class="intro-block">
+        <p class="intro-text">Je m'appelle Clément, j'ai 22 ans, et je suis passionné par le développement informatique, principalement dans le domaine du web. Depuis mon plus jeune âge, je suis fasciné par l'informatique et la manière dont les technologies peuvent transformer le monde. Cela m'a conduit à m'orienter vers une carrière dans le développement, où je peux allier créativité et logique pour résoudre des problèmes complexes.</p>
+        <p class="intro-text">Je suis actuellement en BTS SIO (option SLAM), où je perfectionne mes compétences en programmation, en gestion de bases de données, et en création d'applications web. Mon objectif est de créer des solutions web efficaces et innovantes, tout en continuant à apprendre et à me perfectionner dans ce domaine en constante évolution.</p>
+    </div>
+
+    <div class="journey-block">
+        <h3>Mon parcours scolaire</h3>
+        <div class="timeline">
+            <div class="timeline-item">
+                <span class="year">2014 - 2018</span>
+                <p class="school">Brevet des collèges - Collège Jacques Monod KONACKER</p>
+            </div>
+            <div class="timeline-item">
+                <span class="year">2018 - 2019</span>
+                <p class="school">Seconde générale - Lycée Antoine de Saint-Exupéry FAMECK</p>
+            </div>
+            <div class="timeline-item">
+                <span class="year">2019 - 2021</span>
+                <p class="school">Baccalauréat STI2D (option SIN) - Lycée La Briquerie THIONVILLE</p>
+            </div>
+            <div class="timeline-item">
+                <span class="year">2021</span>
+                <p class="school">BTS SNIR - Lycée La Briquerie THIONVILLE</p>
+            </div>
+            <div class="timeline-item">
+                <span class="year">2023 - 2025</span>
+                <p class="school">BTS SIO (SLAM) - Campus MEWO METZ</p>
+            </div>
         </div>
     </div>
 
-    <p class="conclusion-text">Aujourd'hui, je suis en BTS SIO, spécialité SLAM, où j'approfondis mes compétences en développement web. Mon objectif est de continuer à apprendre et à développer des projets qui allient technologie et innovation.</p>
+    <div class="conclusion-block">
+        <p>Mon parcours en informatique a été marqué par une réelle passion pour les technologies et la volonté de comprendre et maîtriser cet univers fascinant. Aujourd'hui, je suis fier de pouvoir développer des applications et des sites web, et je suis constamment en quête de nouveaux défis pour perfectionner mes compétences.</p>
+    </div>
 
-    <img src="images/moi.jpg" alt="Clément" class="profile-img">
+    <div class="profile-image">
+        <img src="images/moi.jpg" alt="Clément" class="profile-img">
+    </div>
 </div>
+
 <?php include('includes/footer.php'); ?>
 
 <link rel="stylesheet" href="assets/css/about.css">
--- js/about.js
+++ js/about.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
     $('.intro-text').fadeIn(1500);
-    $('.journey-text').fadeIn(2000);
+    
+    $('.journey-block').delay(1000).fadeIn(1500);
 
     $('.timeline-item').each(function(index) {
         $(this).delay(500 * index).fadeIn(1000);
     });
+
+    $('.conclusion-block').delay(2000).fadeIn(1500);
 
     $('.profile-img').delay(3000).fadeIn(1000);
 });

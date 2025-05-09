152eef2ba798f1a851fe60275cb65bd35fbd5863
BobZouille57 <RaiiZeR57@gmail.com>
2025-05-09 10:07:13 +0200

    fix test

--- js/about.js
+++ js/about.js
@@ -1,13 +1,3 @@
 $(document).ready(function() {
-    $('.intro-text').fadeIn(1500);
-
-    $('.journey-block').delay(1000).fadeIn(1500);
-
-    $('.timeline-item').each(function(index) {
-        $(this).delay(500 * index).fadeIn(1000);
-    });
-
-    $('.conclusion-block').delay(2000).fadeIn(1500);
-
-    $('.profile-img').delay(3000).fadeIn(1000);
+    $('.profile-img').fadeIn(1000);
 });
